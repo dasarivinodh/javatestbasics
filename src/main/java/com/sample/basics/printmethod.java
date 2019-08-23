7f220888611653ad39dff64dd7ad0a9bbd2fe07f
Suhas Subbarao <suhas.manju89@gmail.com>
2019-08-23 08:53:12 +0530

    Initial Commit

---
+++ src/main/java/com/sample/basics/printmethod.java
@@ -0,0 +1,14 @@
+package com.sample.basics;
+
+/**
+ * Hello world!
+ *
+ */
+public class printmethod 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( 'A'+'B' );//131
+        
+    }
+}
---
+++ src/main/java/com/sample/basics/threadsexample.java
@@ -0,0 +1,18 @@
+package com.sample.basics;
+
+class threadsexample extends Thread{
+
+    public void run()
+    {
+        System.out.println("Running Thread...");
+    }
+    public static void main(String args[]) {
+        System.out.println("Hello Threads");
+        threadsexample t=new threadsexample();
+        t.start();
+        
+        t.stop();
+        // t.start(); // op will not run run method
+    }
+
+}
